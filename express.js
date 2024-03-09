4cacf2cc4fba40503bf6864d48e5278679dbb0de
RajeshSR0809 <srrajesh.svpur@gmail.com>
2024-03-09 19:37:34 +0530

    adding global error handler and folders

--- express.js
+++ express.js
@@ -16,4 +16,15 @@ app.use(helmet())
 app.use(cors())
 
 
+
+app.use((err, req, res, next) => {
+    if(err.name == "UnauthorizedError"){
+        res.status(401).json({"error": `${err.name} : ${err.message}`});
+    }
+    else if(err){
+        res.status(400).json({"error": `${err.name} : ${err.message}`});
+        console.error(err);
+    }
+})
+
 export default app;
